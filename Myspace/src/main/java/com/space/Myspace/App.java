2c8e82d77c2b9c6a7f5d9b11da48b50eb02272ed
Payal Devi <99740109+PAYALDEVI02@users.noreply.github.com>
2025-07-25 18:57:23 +0530

    New file

---
+++ Myspace/src/main/java/com/space/Myspace/App.java
@@ -0,0 +1,14 @@
+package com.space.Myspace;
+
+
+public class App 
+{
+	 public int add(int a, int b) {
+		 return a+b;
+	 }
+	 public static void main(String args[]) {
+		 App a= new App();
+		 System.out.println("Sum: "+a.add(3,4));
+		 
+	 }
+}
