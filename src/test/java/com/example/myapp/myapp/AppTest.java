2991ffe4dcbfee60e87901566248d8fcabeab41c
Payal Devi <99740109+PAYALDEVI02@users.noreply.github.com>
2025-07-25 16:35:28 +0530

    Update AppTest.java

--- src/test/java/com/example/myapp/myapp/AppTest.java
+++ src/test/java/com/example/myapp/myapp/AppTest.java
@@ -4,16 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
+    
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testAdd() {
+        int result=App.add(5,1);
+        assertEquals(6,result);
     }
 }
