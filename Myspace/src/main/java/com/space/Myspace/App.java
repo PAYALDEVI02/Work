06282b5fbc8a98b32c2633f8cd2e961545605363
Payal Devi <99740109+PAYALDEVI02@users.noreply.github.com>
2025-07-25 19:22:58 +0530

    Update App.java

--- Myspace/src/main/java/com/space/Myspace/App.java
+++ Myspace/src/main/java/com/space/Myspace/App.java
@@ -1,14 +1,10 @@
 package com.space.Myspace;
-
-
-public class App 
-{
-	 public int add(int a, int b) {
-		 return a+b;
-	 }
-	 public static void main(String args[]) {
-		 App a= new App();
-		 System.out.println("Sum: "+a.add(3,4));
-		 
-	 } 
+public class App{
+   public int add(int a, int b) {
+	return a+b;
+   }
+	public static void main(String args[]) {
+		App a= new App();
+		System.out.println("Sum: "+a.add(3,4));
+          } 
 }
