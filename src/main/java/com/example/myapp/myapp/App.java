71f8c456a0802d8c0f2f66e8e5d43e502124c8df
Payal Devi <99740109+PAYALDEVI02@users.noreply.github.com>
2025-07-25 17:47:01 +0530

    Update App.java

--- src/main/java/com/example/myapp/myapp/App.java
+++ src/main/java/com/example/myapp/myapp/App.java
@@ -3,7 +3,7 @@ public class App{
 public sttaic void main(String args[]){
    App a=new App();
    int sum=a.sum(1,2);
-   System.out.println("Sum: "+sum)
+   System.out.println("Sum: "+sum);
 }
    public int sum(int a, int b){
       return a+b;
