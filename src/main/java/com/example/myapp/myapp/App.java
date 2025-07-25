f9f71328a039a0f7e1937c9032371b3ae8aee2b8
Payal Devi <99740109+PAYALDEVI02@users.noreply.github.com>
2025-07-25 17:12:18 +0530

    Update App.java

--- src/main/java/com/example/myapp/myapp/App.java
+++ src/main/java/com/example/myapp/myapp/App.java
@@ -5,7 +5,9 @@ package com.example.myapp.myapp;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       public static int add(int a, int b){
+           return a+b;
+       }
        
     }
 }
