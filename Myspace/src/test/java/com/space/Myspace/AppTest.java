61287d021bf470a3d7b8d8c8e7ec183aba87c7d7
Payal Devi <99740109+PAYALDEVI02@users.noreply.github.com>
2025-07-28 15:12:02 +0530

    Update AppTest.java

--- Myspace/src/test/java/com/space/Myspace/AppTest.java
+++ Myspace/src/test/java/com/space/Myspace/AppTest.java
@@ -1,11 +1,10 @@
 package com.space.Myspace;
-
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 import org.junit.jupiter.api.Test;
-
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+/**
+* Unit test for simple App.
+*/
 public class AppTest {
    /**
     * Rigorous Test :-)
@@ -14,6 +13,9 @@ public class AppTest {
    public void testMessage() {
        String expected = "Hello World";
        assertEquals(expected, App.getMessage());
+   }
+   public void testSubtract(){
+       assertEquals(2,App.subtract(5,3));
    }
     public void testMultiplication(){
        assertEquals(14,App.multiply(5,3));
