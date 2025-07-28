0795da7c54fb7e758447b7798a28941f9d5217a7
Payal Devi <99740109+PAYALDEVI02@users.noreply.github.com>
2025-07-28 13:16:51 +0530

    Update2 App.java

--- Myspace/src/main/java/com/space/Myspace/App.java
+++ Myspace/src/main/java/com/space/Myspace/App.java
@@ -3,8 +3,12 @@ public class App{
    public int add(int a, int b) {
 	return a+b;
    }
+	public int sub(int a, int b){
+		return a-b;
+	}
 	public static void main(String args[]) {
 		App a= new App();
 		System.out.println("Sum: "+a.add(3,4));
+		System.out.println("Subtraction: "+a.sub(8,5))
           } 
 }
