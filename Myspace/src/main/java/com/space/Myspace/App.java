16bde29dcd7263d4642c7252a83a881a9aef5e01
Payal Devi <99740109+PAYALDEVI02@users.noreply.github.com>
2025-08-22 17:58:06 +0530

    Update App.java

--- Myspace/src/main/java/com/space/Myspace/App.java
+++ Myspace/src/main/java/com/space/Myspace/App.java
@@ -1,8 +1,9 @@
 package com.space.Myspace;
-public class App{
-   public static void main(String args[]){
-	   System.out.println("Hellooo");
-   }
+
+public class App {
+    public int mul(int a, int b) {
+        return a * b;
+    }
 }
 
 
