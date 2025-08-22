fc6b0c1009c15e84c48d0fe6d68effaf56ec2856
Payal Devi <99740109+PAYALDEVI02@users.noreply.github.com>
2025-08-22 17:40:10 +0530

    Update AppTest.java

--- Myspace/src/test/java/com/space/Myspace/AppTest.java
+++ Myspace/src/test/java/com/space/Myspace/AppTest.java
@@ -1,31 +1,12 @@
-package com.space.Myspace;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 import org.junit.jupiter.api.Test;
 
-public class AppTest 
-    extends TestCase
-{
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-	
-    public static Test suite() 
-    {
-        return new TestSuite( AppTest.class );
-    }
-     public void testMul() {
-	App a=new App();
-	int res=a.mul(4,5);
-	assertEquals(22,res);
-     }
+public class AppTest {
 
+    @Test
+    public void testMul() {
+        App a = new App();
+        int res = a.mul(4, 5);
+        assertEquals(22, res);
+    }
 }
-
-
-
